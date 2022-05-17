227e9a6c3705b869b1343b09ddda93c6d4e34de1
crismoute <cristianmotaramirez92@gmail.com>
2022-05-17 15:02:40 -0500

    Se creo carpeta styles, se creo el archivo testimonio.js y se modificaron estilos de archivo app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import Testimonio from './components/Testimonio.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio />
+      </div>  
     </div>
   );
 }
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -5,7 +5,16 @@ function Testimonio(){
         <div className='contenedor-testimonio'>
             <img 
                 className='imagen-testimonio'
-                src={require('../img/test-cris.jpg')}/>
+                src={require('../img/test-cris.jpg')}
+                alt='Foto de Cris'/>
+
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>Cristian Mota en México</p>
+                <p className='cargo-testimonio'>Comunicólogo | Web Developer</p>
+                <p className='texto-testimonio'>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p> 
+            </div>    
         </div>
+   
     );
 }
+export default Testimonio;
