68dfe747c3c0da392a3b4951bf238ee1579a8b96
crismoute <cristianmotaramirez92@gmail.com>
2022-05-17 15:48:15 -0500

    se crearon las tres tarjetas con props utilizando testimonio.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio 
+          nombre= 'Cristian Mota'
+          pais= 'México'
+          imagen= 'cris'
+          cargo= 'Web Developer'
+          empresa= 'Spotify'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+        />
+         <Testimonio 
+          nombre= 'Enrique Centeno'
+          pais= 'México'
+          imagen= 'enrique'
+          cargo= 'Creative Jr.'
+          empresa= 'Cartel'
+          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+        />
+          <Testimonio 
+          nombre= 'Alejandro Avila'
+          pais= 'México'
+          imagen= 'alex'
+          cargo= 'Location Manager'
+          empresa= 'Loft 450'
+          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+        />
       </div>  
     </div>
   );
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../styles/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
     return(
         <div className='contenedor-testimonio'>
             <img 
                 className='imagen-testimonio'
-                src={require('../img/test-cris.jpg')}
+                src={require(`../img/test-${props.imagen}.jpg`)}
                 alt='Foto de Cris'/>
 
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>Cristian Mota en México</p>
-                <p className='cargo-testimonio'>Comunicólogo | Web Developer</p>
-                <p className='texto-testimonio'>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p> 
+                <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+                <p className='cargo-testimonio'>{props.cargo} | {props.empresa}</p>
+                <p className='texto-testimonio'>{props.testimonio}</p> 
             </div>    
         </div>
    
